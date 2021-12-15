80bafe478a60d118b4edbb92b8225a766af5642a
Yohannes Taffese <yohannestaffese12@gmail.com>
2021-12-16 00:33:09 +0100

    worked on signIn UI

--- src/Login.js
+++ src/Login.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
+import SignupScreen from "./SignupScreen";
 function Login() {
+  const [signIn, setSignIn] = useState(false);
+
   return (
     <div className="loginScreen">
       <div className="loginScreen_background">
@@ -10,11 +13,37 @@ function Login() {
           alt=""
         />
 
-        <button className="loginScreen_button">Sign in</button>
+        <button onClick={() => setSignIn(true)} className="loginScreen_button">
+          Sign in
+        </button>
         <div className="loginScreen_gradient" />
       </div>
 
-      <div className="loginScreen_body"></div>
+      <div className="loginScreen_body">
+        {signIn ? (
+          <SignupScreen />
+        ) : (
+          <>
+            <h1>Unlimited films, TV programs and more.</h1>
+            <h2>watch anywhere, Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+            <div className="loginScreen_input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginScreen_getStarted"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
